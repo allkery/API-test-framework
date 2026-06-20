081ff5e0475cbd5ebad1777871855a57b374d06b
allkery <dev.afedorov@gmail.com>
2026-06-20 17:19:41 +0300

    feat: add test_comments

---
+++ tests/test_comments.py
@@ -0,0 +1,29 @@
+import pytest
+from models.comment import CommentSchema
+
+
+def test_get_comments(api_client):
+    """получить список всех комментариев"""
+
+    response = api_client.get("/comments")
+
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+    for comment in response.json():
+        CommentSchema(**comment)
+
+
+@pytest.mark.parametrize(
+        "post_id",
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    )
+def test_get_comments_for_post(api_client, post_id):
+    """получить комментарии для поста"""
+
+    response = api_client.get(f"/posts/{post_id}/comments")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+    for comment in response.json():
+        CommentSchema(**comment)
