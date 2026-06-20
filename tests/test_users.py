1f715428935adb9c5e9ab6d2d50a92e3f7a13f45
allkery <dev.afedorov@gmail.com>
2026-06-20 20:29:06 +0300

    refactor: add assertions utils

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,13 +1,15 @@
 import pytest
 from models.user import UserSchema
+from utils.assertions import assert_list_not_empty, assert_status_code
+
 
 def test_get_all_users(api_client):
     """получитить список всех пользователей"""
     
     response = api_client.get("/users")
 
-    assert response.status_code == 200
-    assert len(response.json()) > 0
+    assert_status_code(response, 200)
+    assert_list_not_empty(response)
 
 
 @pytest.mark.parametrize("user_id", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
@@ -17,5 +19,5 @@ def test_get_user_by_id(api_client, user_id):
     response = api_client.get(f"/users/{user_id}")
     user = UserSchema(**response.json())
 
-    assert response.status_code == 200
+    assert_status_code(response, 200)
     assert user.id == user_id
