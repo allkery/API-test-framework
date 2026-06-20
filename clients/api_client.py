50ab076f738c4370dffe0065140caaa64f24f777
allkery <dev.afedorov@gmail.com>
2026-06-20 23:21:47 +0300

    feat: add base_url to APIClient

--- clients/api_client.py
+++ clients/api_client.py
@@ -3,7 +3,7 @@ from dotenv import dotenv_values
 
 
 class APIClient:
-    def __init__(self, base_url=dotenv_values(".env").get("BASE_URL")):
+    def __init__(self, base_url='https://jsonplaceholder.typicode.com'):
         self.base_url = base_url
         self.session = requests.Session()
 
