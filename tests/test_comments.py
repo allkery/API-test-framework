f32b769310ac44ef1a85464573dfa68adecbdcf0
allkery <dev.afedorov@gmail.com>
2026-06-20 21:00:14 +0300

    feat: add allure steps to test_comments

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -1,20 +1,30 @@
 import pytest
 from models.comment import CommentSchema
 from utils.assertions import assert_status_code, assert_list_not_empty
+import allure
 
 
+@allure.title("Получить список комментариев")
 def test_get_comments(api_client):
     """получить список всех комментариев"""
 
-    response = api_client.get("/comments")
+    with allure.step("Отправить GET /comments"):
+        response = api_client.get("/comments")
+        allure.attach(response.text, name="Response body",
+            attachment_type=allure.attachment_type.JSON)
+        
+    with allure.step("Проверить статус код 200"):
+        assert_status_code(response, 200)
 
-    assert_status_code(response, 200)
-    assert_list_not_empty(response)
+    with allure.step("Проверить, что список не пуст"):
+        assert_list_not_empty(response)
 
-    for comment in response.json():
-        CommentSchema(**comment)
+    with allure.step("Валидировать элементы ответа"):
+        for comment in response.json():
+            CommentSchema(**comment)
 
 
+@allure.title("Получить комментарии для поста")
 @pytest.mark.parametrize(
         "post_id",
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -22,10 +32,17 @@ def test_get_comments(api_client):
 def test_get_comments_for_post(api_client, post_id):
     """получить комментарии для поста"""
 
-    response = api_client.get(f"/comments?postId={post_id}")
+    with allure.step(f"Отправить GET /comments?postId={post_id}"):
+        response = api_client.get(f"/comments?postId={post_id}")
+        allure.attach(response.text, name="Response body",
+            attachment_type=allure.attachment_type.JSON)
 
-    assert_status_code(response, 200)
-    assert_list_not_empty(response)
+    with allure.step("Проверить статус код 200"):
+        assert_status_code(response, 200)
 
-    for comment in response.json():
-        CommentSchema(**comment)
+    with allure.step("Проверить, что список не пуст"):
+        assert_list_not_empty(response)
+
+    with allure.step("Валидировать элементы ответа"):
+        for comment in response.json():
+            CommentSchema(**comment)
