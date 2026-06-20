3479c497bcc1fb215e6f9b6ff7aba1aa09384345
allkery <dev.afedorov@gmail.com>
2026-06-20 21:04:44 +0300

    feat: add allure steps to test_albums

--- tests/test_albums.py
+++ tests/test_albums.py
@@ -2,20 +2,30 @@ import pytest
 from models.album import AlbumSchema
 from models.photo import PhotoSchema
 from utils.assertions import assert_list_not_empty, assert_status_code
+import allure
 
 
+@allure.title("Получить список альбомов")
 def test_get_albums(api_client):
     """получить список всех альбомов"""
 
-    response = api_client.get("/albums")
+    with allure.step("Отправить GET /albums"):
+        response = api_client.get("/albums")
+        allure.attach(response.text, name="Response body",
+            attachment_type=allure.attachment_type.JSON)
 
-    assert_status_code(response, 200)
-    assert_list_not_empty(response)
+    with allure.step("Проверить статус код 200"):
+        assert_status_code(response, 200)
 
-    for album in response.json():
-        AlbumSchema(**album)
+    with allure.step("Проверить, что список не пуст"):
+        assert_list_not_empty(response)
+
+    with allure.step("Валидировать элементы ответа"):
+        for album in response.json():
+            AlbumSchema(**album)
 
 
+@allure.title("Получить альбом по айди")
 @pytest.mark.parametrize(
         "album_id",
         [1, 5, 10, 15, 20, 25, 30, 35, 40, 45]
@@ -23,15 +33,23 @@ def test_get_albums(api_client):
 def test_get_album_by_id(api_client, album_id):
     """получить альбом по его ID"""
 
-    response = api_client.get(f"/albums/{album_id}")
+    with allure.step("Отправить GET /albums/{album_id}"):
+        response = api_client.get(f"/albums/{album_id}")
+        allure.attach(response.text, name="Response body",
+                attachment_type=allure.attachment_type.JSON)
 
-    assert_status_code(response, 200)
-    assert_list_not_empty(response)
+    with allure.step("Проверить статус код 200"):
+        assert_status_code(response, 200)
 
-    validated = AlbumSchema(**response.json())
-    assert validated.id == album_id
+    with allure.step("Проверить, что список не пуст"):
+        assert_list_not_empty(response)
+
+    with allure.step("Валидировать элементы ответа"):
+        validated = AlbumSchema(**response.json())
+        assert validated.id == album_id
 
 
+@allure.title("Получчить список всех альбомов пользователя")
 @pytest.mark.parametrize(
         "user_id",
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -39,16 +57,22 @@ def test_get_album_by_id(api_client, album_id):
 def test_get_albums_by_user_id(api_client, user_id):
     """получить список всех альбомов пользователя"""
 
-    response = api_client.get(f"/users/{user_id}/albums")
+    with allure.step("Отправить GET /users/{user_id}/albums"):
+        response = api_client.get(f"/users/{user_id}/albums")
 
-    assert_status_code(response, 200)
-    assert_list_not_empty(response)
+    with allure.step("Проверить статус код 200"):
+        assert_status_code(response, 200)
 
-    for album in response.json():
-        validated = AlbumSchema(**album)
-        assert validated.userId == user_id
+    with allure.step("Проверить, что список не пуст"):
+        assert_list_not_empty(response)
+
+    with allure.step("Валидировать элементы ответа"):
+        for album in response.json():
+            validated = AlbumSchema(**album)
+            assert validated.userId == user_id
 
 
+@allure.title("получить список всех фотографий альбома")
 @pytest.mark.parametrize(
         "album_id",
         [1, 5, 10, 15, 20, 25, 30, 35, 40, 45]
@@ -56,10 +80,15 @@ def test_get_albums_by_user_id(api_client, user_id):
 def test_get_photos_for_album(api_client, album_id):
     """получить список всех фотографий альбома"""
 
-    response = api_client.get(f"/albums/{album_id}/photos")
+    with allure.step("Отправить GET /albums/{album_id}/photos"):
+        response = api_client.get(f"/albums/{album_id}/photos")
 
-    assert_status_code(response, 200)
-    assert_list_not_empty(response)
+    with allure.step("Проверить статус код 200"):
+        assert_status_code(response, 200)
 
-    for photo in response.json():
-        PhotoSchema(**photo)
+    with allure.step("Проверить, что список не пуст"):
+        assert_list_not_empty(response)
+
+    with allure.step("Валидировать элементы ответа"):
+        for photo in response.json():
+            PhotoSchema(**photo)
